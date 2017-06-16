0dd00af22a39ee49134052f00dca31f01450f951
Lou Foster <lf6f@virginia.edu>
2017-06-16 10:08:57 -0400

    dev mode auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
    def authorize
       if session[:user_type] == "staff"
          computing_id = request.env['HTTP_REMOTE_USER'].to_s
-         # if computing_id.blank? && Rails.env != "production"
-         #    computing_id = Figaro.env.dev_test_user
-         # end
+         if computing_id.blank? && Rails.env != "production"
+            computing_id = Figaro.env.dev_test_user
+         end
          redirect_to "/unauthorized" and return if computing_id.nil?
 
          @curr_user = User.find_by(computing_id: computing_id)
