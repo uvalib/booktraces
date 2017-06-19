9d7093fd970b425618c5a0182228217fc5b4e9a4
Lou Foster <lf6f@virginia.edu>
2017-06-19 14:56:58 -0400

    basic sort/filter. expose to public and admin

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,29 +1,66 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function() {
+   $(".chosen-select").chosen({
+       no_results_text: "Sorry, no matches found",
+       width: "100%"
+   });
+
    // Columns in the table:
    //   ID, CallNum, Title, Bookplate, Library, class, subclass, intervention
-   $('#shelf-listings').DataTable( {
+   var table = $('#shelf-listings').DataTable( {
       serverSide: true,
       processing: true,
       pageLength: 50,
       ordering: false,
-      searching: false,
       columnDefs: [
-         { "width": "50px", "targets": [0] },
-         { "width": "30px", "targets": [5,6] },
-         { "width": "75px", "targets": [1] },
-         { "width": "100px", "targets": [2] }
+         { width: "50px", targets: [0] },
+         { width: "30px", targets: [5,6] },
+         { width: "70px", targets: [1] },
+         { render: function ( data, type, row ) {
+            var url = "http://search.lib.virginia.edu/catalog?catalog_select=catalog&search_field=advanced&call_number=";
+            var safe = data.replace(/\s/g, "+");
+            url += safe;
+            return "<a title='View in Virgo' target='_blank' href='"+url+"'>"+data+"</a>";
+         }, targets: 2}
       ],
       ajax: {
         url:  '/api/query',
         type: 'POST'
     }
    });
-   var table = $('#example').DataTable();
 
-   $(".chosen-select").chosen({
-       no_results_text: "Sorry, no matches found",
-       width: "100%"
+   // virgo link fmt;
+   // http://search.lib.virginia.edu/catalog?call_number=BV4253+.R8+1853&catalog_select=catalog&search_field=advanced
+
+   var doFilter = function() {
+      var interventions = $("#intervention-filter").is(":checked");
+      table.columns(5).search(  $("#library-filter").val() );
+      table.columns(6).search( $("#class-filter").val() );
+      table.columns(7).search( $("#subclass-filter").val() );
+
+      var q = $("#query").val();
+      $("#shelf-listings_filter input").val(q);
+      table.search( q );
+
+      table.draw();
+   };
+
+   $("#query").on("keyup", function (evt) {
+      if ( evt.keyCode === 13) {
+         doFilter();
+      }
+   });
+
+   $("#clear").on("click", function() {
+      $("select.filter").val("Any");
+      $('select.filter').trigger("chosen:updated");
+      $("#query").val("");
+      $("#intervention-filter").prop("checked", false);
+      table.search("").draw();
+   });
+
+   $("#filter").on("click", function() {
+      doFilter();
    });
 });
--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,12 +1,44 @@
 class Api::ListingsController < ApplicationController
    # POST query request from datatables
+   # Key stuff to look for:
+   #   params[:search][:value] = 'global' search
+   #   params[:columns][5, 6 or 7][:search][:value]
    def query
-
       draw = params[:draw].to_i
       total = ShelfListing.count
-      data = []
+      filtered = total
+
+      query_terms = []
+      q = params[:search]["value"]
+      if !q.blank?
+         query_terms << "(title like '%#{q}%' or call_number like '%#{q}%' or bookplate_text like '%#{q}%')"
+      end
+      lib_filter = params[:columns]["5"][:search][:value]
+      if !lib_filter.blank? && lib_filter != "Any"
+         query_terms << "library = '#{lib_filter}'"
+      end
+
+      class_filter = params[:columns]["6"][:search][:value]
+      if !class_filter.blank? && class_filter != "Any"
+         query_terms << "classification = '#{class_filter}'"
+      end
+
+      subclass_filter = params[:columns]["7"][:search][:value]
+      if !subclass_filter.blank? && subclass_filter != "Any"
+         query_terms << "subclassification = '#{subclass_filter}'"
+      end
+
       # Table: ID, BARCODE, CallNum, Title, Bookplate, Library, class, subclass, intervention
-      ShelfListing.offset(params[:start]).limit(params[:length]).includes(:barcodes).includes(:interventions).each do |sl|
+      data = []
+      if query_terms.empty?
+         res = ShelfListing.includes(:barcodes).includes(:interventions).offset(params[:start]).limit(params[:length])
+      else
+         q_str = query_terms.join(" and ")
+         filtered = ShelfListing.where(q_str).count
+         res = ShelfListing.includes(:barcodes).includes(:interventions).where(q_str).offset(params[:start]).limit(params[:length])
+      end
+
+      res.each do |sl|
          bc = sl.active_barcodes.join(",")
          flag = !sl.interventions.empty?
          data << [
@@ -16,7 +48,7 @@ class Api::ListingsController < ApplicationController
       end
 
       # note: only set filtered different from total if there is some query made
-      resp = { draw: draw, recordsTotal: total, recordsFiltered: total, data: data}
+      resp = { draw: draw, recordsTotal: total, recordsFiltered: filtered, data: data}
       render json: resp
    end
 end
