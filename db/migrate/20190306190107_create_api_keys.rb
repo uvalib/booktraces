74001c90fb5a1165dabac0cd93846d1e1b5f3be0
Lou Foster <lf6f@virginia.edu>
2019-03-13 13:06:10 -0400

    break migration for api key into up/down create bt key on up

--- db/migrate/20190306190107_create_api_keys.rb
+++ db/migrate/20190306190107_create_api_keys.rb
@@ -1,13 +1,21 @@
 class CreateApiKeys < ActiveRecord::Migration[5.2]
-  def change
-    create_table :api_keys do |t|
-      t.string :email, unique: true, null: false
-      t.string :first_name
-      t.string :last_name
-      t.string :institution
-      t.string :key, index: {unique: true}, null: false
-      t.boolean :active, default: true
-      t.timestamps
-    end
-  end
+   def change
+      reversible do |dir|
+         dir.up do
+            create_table :api_keys do |t|
+              t.string :email, unique: true, null: false
+              t.string :first_name
+              t.string :last_name
+              t.string :institution
+              t.string :key, index: {unique: true}, null: false
+              t.boolean :active, default: true
+              t.timestamps
+            end
+            ApiKey.create(email:"booktraces",first_name:"book", last_name: "traces", institution: "UVA")
+         end
+         dir.down do
+            drop_table :api_keys
+         end
+      end
+   end
 end
