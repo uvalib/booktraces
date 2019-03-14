6e17a87fe237457283e75d72e21543f034bda32f
UVADave <dpg3k@virginia.edu>
2019-03-14 08:05:32 -0400

    Add a migrate to the startup script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,8 @@
 # remove stale pid files
 rm -f $APP_HOME/tmp/pids/server.pid > /dev/null 2>&1
 
+# run a migration if necessary
+rake db:migrate
+
 # run the server
 rails server -b 0.0.0.0 -p 8080
